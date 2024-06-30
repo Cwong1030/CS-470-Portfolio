44df18681f4ab425942506d7c9b72765f281cba7
Cwong1030 <111389879+Cwong1030@users.noreply.github.com>
2024-06-29 17:36:14 -0700

    Add files via upload

---
+++ Module Two Assignment/lafs-web/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_url: 'https://80bvhacm73.execute-api.us-east-1.amazonaws.com'
+};
+
+// CS-470: Project One - Chris Wong 06/13/2024
---
+++ Module Two Assignment/lafs-web/src/main.ts
@@ -0,0 +1,20 @@
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { LoopBackConfig } from '../sdk';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+LoopBackConfig.setBaseURL(environment.api_url);
+
+LoopBackConfig.filterOnUrl();
+
+// CS-470: Project One - Chris Wong 06/13/2024
